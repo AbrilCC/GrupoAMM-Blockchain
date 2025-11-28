e3054328ba355ef0758b7cfa8e2e4be488b2d6b0
AbrilCC <abrilelisiricc@gmail.com>
2025-11-28 02:03:23 -0300

    addLiquidity on Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract AMM {
+    uint256 supplyETH;
+    uint256 supplyToken;
+    uint256 supplyLiquidity;
+    uint256 k;
+
+    function addLiquidity() public {
+        uint256 alpha = msg.value / supplyETH;
+        
+        supplyETH =+ (alpha + 1)*supplyETH;
+        supplyToken =+ (alpha + 1)*supplyToken;
+        supplyLiquidity =+ (alpha + 1)*supplyLiquidity;
     }
 
-    function increment() public {
-        number++;
+    function removeLiquidity(uint256 amount) public {
+
+    }
+
+    function getETHPrice(uint256 amountToSpend) {
+        
+    }
+
+    function getUNItokenPrice(uint256 amountToSpend) {
+        
     }
 }
