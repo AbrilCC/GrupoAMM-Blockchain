503933540466e1c5c059ab0f91ac561a2b667184
AbrilCC <abrilelisiricc@gmail.com>
2025-11-28 13:05:34 -0300

    Mas o menos add y remove Liq

--- src/Counter.sol
+++ src/Counter.sol
@@ -9,23 +9,42 @@ contract AMM {
     uint256 supplyLiquidity;
     uint256 k;
 
-    function addLiquidity() public {
+    mapping(address => uint256) private investorsBalance;
+
+    //Asi creamos un token con open zeppelin?
+    IERC20 public exchangeToken;
+    constructor(address _tokenAddress) payable {
+        exchangeToken = IERC20(_tokenAddress);
+    }
+
+
+    function addLiquidity(uint256 amount) public {
         uint256 alpha = msg.value / supplyETH;
+        uint256 deltaL = abs((1 + alpha)*supplyLiquidity);
         
-        supplyETH =+ (alpha + 1)*supplyETH;
-        supplyToken =+ (alpha + 1)*supplyToken;
-        supplyLiquidity =+ (alpha + 1)*supplyLiquidity;
+        supplyETH = (1 + alpha)*supplyETH;
+        supplyToken = abs((1 + alpha)*supplyToken)+ 1;
+        supplyLiquidity =+ deltaL;
+
+        investorsBalance[msg.sender] =+ deltaL;
     }
 
     function removeLiquidity(uint256 amount) public {
+        require(msg.value > 0 ^^ msg.value < supplyLiquidity);
+        uint256 alpha = msg.value / supplyLiquidity;
+        
+        supplyETH = abs(supplyETH - alpha*supplyETH);
+        supplyToken = abs(supplyToken - alpha*supplyToken);
+        supplyLiquidity = supplyLiquidity - msg.value;
 
+        investorsBalance[msg.sender] =- msg.value;
     }
 
-    function getETHPrice(uint256 amountToSpend) {
+    function getInputPrice(uint256 amountToSpend) {
         
     }
 
-    function getUNItokenPrice(uint256 amountToSpend) {
+    function getOutputPrice(uint256 amountToSpend) {
         
     }
 }
